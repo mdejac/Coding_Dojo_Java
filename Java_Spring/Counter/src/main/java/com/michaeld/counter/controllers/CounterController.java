5ed9b795c3b96941abe06abec97080b7e13d250c
mdejac <dejacm@gmail.com>
2023-06-21 18:11:46 -0400

    Counter commit

---
+++ Java_Spring/Counter/src/main/java/com/michaeld/counter/controllers/CounterController.java
@@ -0,0 +1,34 @@
+package com.michaeld.counter.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class CounterController {
+
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String index(HttpSession session) {
+		if (session.getAttribute("visitCount") == null) {
+			session.setAttribute("visitCount", 0);
+		} else {
+			session.setAttribute("visitCount", (Integer)session.getAttribute("visitCount")+1);
+		}
+		
+		return "index.jsp";
+	}
+	
+	@RequestMapping("/counter")
+	public String counterDisplay() {
+		return "counterDisplay.jsp";
+	}
+	
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public String resetCounter(HttpSession session) {
+		session.setAttribute("visitCount", 0);
+		return "index.jsp";
+	}
+	
+}
---
+++ Java_Spring/Counter/src/main/webapp/WEB-INF/counterDisplay.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Current visit count</title>
+</head>
+<body>
+	<p>You have visited localhost:8080 
+		<c:choose>
+			<c:when test="${visitCount==null}">
+				0
+			</c:when>
+			<c:otherwise>
+				<c:out value="${visitCount + 1}"/>
+			</c:otherwise>
+		</c:choose>
+	 times.</p>
+	<a href="/">Test another visit.</a>
+</body>
+</html>
